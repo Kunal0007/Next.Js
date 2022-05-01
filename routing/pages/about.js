a3c60b91342b5a5323a35d2de2f3c1bf12757471
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-02 00:41:28 +0530

    first commit

---
+++ routing/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About You? </div>
+  )
+}
+
+export default About
---
+++ routing/pages/blogs/Mast.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mast = () => {
+  return (
+    <div>Mast!!!!!!!!!!!!!</div>
+  )
+}
+
+export default Mast
