96e9e07b516f4c403a91088b23edadc77252492b
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-03 00:08:02 +0530

    Blog Update

--- myblogs/pages/index.js
+++ myblogs/pages/index.js
@@ -14,6 +14,14 @@ export default function Home() {
 
       {/* <Script src='' ></Script> */}
 
+      <nav>
+        <ul className={styles.navbar}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem}>Blogs</li>
+          <li className={styles.navItem}>About</li>
+        </ul>
+      </nav>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to MY BLOG
@@ -23,14 +31,16 @@ export default function Home() {
           Blogs With My Experience!!
         </p>
 
+        <h3>Popular Blogs</h3>
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Blog 1 &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>Blog 2 &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -38,7 +48,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2>Blog 3 &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -46,7 +56,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Blog 4 &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
