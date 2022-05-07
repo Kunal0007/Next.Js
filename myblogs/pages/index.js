dcdf59551c9734bf6fc212b74f78fedd33384539
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-07 19:57:15 +0530

    Update

--- myblogs/pages/index.js
+++ myblogs/pages/index.js
@@ -26,12 +26,46 @@ export default function Home() {
         <h3>Popular Blogs</h3>
 
         <div className={styles.grid}>
-        <Link href='/blogpost/How-to-Integrate-Java-with-SQL'>
-        <a className={styles.card}>
-          <h2>Blog 1 &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-      </Link>
+          <Link href='/blogpost/How to learn Next.js'>
+            <a className={styles.card}>
+              <h2>Blog 1 &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </Link>
+
+          <Link href='/blogpost/How to Integrate Java with SQL'>
+            <a className={styles.card}>
+              <h2>Blog 2 &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
+          </Link>
+
+          <Link href='/blogpost/How to learn Cloud Computing'>
+            <a className={styles.card} >
+              <h2>Blog 3 &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
+          </Link>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Blog 4 &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+
+
+        {/* <div className={styles.grid}>
+          <Link href='/blogpost/How-to-Integrate-Java-with-SQL'>
+            <a className={styles.card}>
+              <h2>Blog 1 &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Blog 2 &rarr;</h2>
@@ -55,7 +89,8 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
+
       </main>
 
     </div>
