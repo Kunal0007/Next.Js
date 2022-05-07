b03d258a7ec937929c3af49e078c891b5723250e
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-07 19:57:08 +0530

    Update

--- myblogs/pages/blogs.js
+++ myblogs/pages/blogs.js
@@ -6,25 +6,27 @@ import styles from '../styles/Blogs.module.css'
 const Blogs = () => {
   return (
     <div className={styles.grid}>
-      <Link href='/blogpost/How-to-Integrate-Java-with-SQL'>
+      <Link href='/blogpost/How to learn Next.js'>
         <a className={styles.card}>
           <h2>Blog 1 &rarr;</h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
       </Link>
-      <a href="https://nextjs.org/learn" className={styles.card}>
-        <h2>Blog 2 &rarr;</h2>
-        <p>Learn about Next.js in an interactive course with quizzes!</p>
-      </a>
 
-      <a
-        href="https://github.com/vercel/next.js/tree/canary/examples"
-        className={styles.card}
-      >
-        <h2>Blog 3 &rarr;</h2>
-        <p>Discover and deploy boilerplate example Next.js projects.</p>
-      </a>
+      <Link href='/blogpost/How to Integrate Java with SQL'>
+        <a className={styles.card}>
+          <h2>Blog 2 &rarr;</h2>
+          <p>Learn about Next.js in an interactive course with quizzes!</p>
+        </a>
+      </Link>
 
+      <Link href='/blogpost/How to learn Cloud Computing'>
+        <a className={styles.card} >
+          <h2>Blog 3 &rarr;</h2>
+          <p>Discover and deploy boilerplate example Next.js projects.</p>
+        </a>
+      </Link>
+      
       <a
         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         className={styles.card}
