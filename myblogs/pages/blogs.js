ce1cb2daeb4c5ba2cc2eb6d8f0aa3a042c1ed320
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-08 22:52:47 +0530

    fetch Apis

--- myblogs/pages/blogs.js
+++ myblogs/pages/blogs.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Blogs.module.css'
 
 const Blogs = () => {
+
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/blogs').then((data) => {
+      return data.json();
+    }).then((data) => {
+      // console.log(data);
+      setBlogs(data)
+    })
+  }, [])
+
+
   return (
     <div className={styles.grid}>
-      <Link href='/blogpost/How to learn Next.js'>
+      {blogs.map(({ title, author, content }) => {
+        return (
+            <Link href={`/blogpost/${title}`} key={title}>
+              <a className={styles.card}>
+                <h2>{title} &rarr;</h2>
+                <p>{content.substr(0, 50)}</p>
+              </a>
+            </Link>
+        );
+      })}
+      {/* <Link href='/blogpost/How to learn Next.js'>
         <a className={styles.card}>
           <h2>Blog 1 &rarr;</h2>
           <p>Find in-depth information about Next.js features and API.</p>
@@ -25,8 +47,8 @@ const Blogs = () => {
           <h2>Blog 3 &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
-      </Link>
-      
+      </Link> 
+
       <a
         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         className={styles.card}
@@ -35,7 +57,7 @@ const Blogs = () => {
         <p>
           Instantly deploy your Next.js site to a public URL with Vercel.
         </p>
-      </a>
+      </a>*/}
     </div>
   )
 }
