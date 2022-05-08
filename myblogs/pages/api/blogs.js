0357209364028d3ccfc9e4e07fbfab5fe1e8bf3d
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-08 22:53:25 +0530

    generate API

--- myblogs/pages/api/blogs.js
+++ myblogs/pages/api/blogs.js
@@ -1,10 +1,13 @@
 import * as fs from 'fs'
 
-export default function handler(req, res) {
-    fs.readdir('blogdata', 'utf-8',
-        (err, data) => {
-            console.log('');
-            res.status(200).json(data) 
-        }
-    ) 
+export default async function handler(req, res) {
+    let data = await fs.promises.readdir('blogdata', 'utf-8')
+    let file;
+    let blogposts = [];
+    for (let index = 0; index < data.length; index++) {
+        const item = data[index];
+        file = await fs.promises.readFile(('blogdata/' + item), 'utf-8');
+        blogposts.push(JSON.parse(file));
+    }
+    res.status(200).json(blogposts)
 }
