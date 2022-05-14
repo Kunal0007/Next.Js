c2588ecb1567871f8d844c1d74adf579bd294a94
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-14 23:46:42 +0530

    contact api

--- myblogs/pages/api/postContact.js
+++ myblogs/pages/api/postContact.js
@@ -1,24 +1,11 @@
-export default function handler(req, res) {
-    if (req.method === 'POST'){
-        const data = JSON.parse(req.body);
-        const { name, email, message } = data;
-        const contact = new Contact({
-            name,
-            email,
-            message
-        });
-        contact.save()
-            .then(() => {
-                res.status(200).json({
-                    message: 'contact saved'
-                });
-            }
-            )
-            .catch(err => {
-                res.status(500).json({
-                    error: err
-                });
-            }
-            );
+import * as fs from 'fs';
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        let data = await fs.promises.readdir('contactdata', 'utf-8');
+        let contact = await fs.promises.writeFile(`contactdata/${data.length + 1}.json`, JSON.stringify(req.body))
+        res.status(200).json(req.body);
     }
-  }
+    else {
+        res.status(200).json("Hello World");
+    }
+}
