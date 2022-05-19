a89d5ca8bcd3f1f0480eafdd4423510f02f19980
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-20 00:10:58 +0530

    About Page

--- myblogs/pages/about.js
+++ myblogs/pages/about.js
@@ -5,6 +5,23 @@ const About = () => {
   return (
     <div className={styles.main}>
         <h1>About</h1>
+        <h2>Introduction</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui commodi, quia ea quo quasi atque fugit laboriosam cum sequi nostrum similique, enim saepe id fugiat temporibus aliquam culpa totam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur eveniet doloremque eius rerum impedit tempora accusamus saepe, incidunt, fugit atque voluptatem veritatis officia asperiores et? Ab tempora recusandae sunt amet?</p>
+
+        <h2>What is this?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui commodi, quia ea quo quasi atque fugit laboriosam cum sequi nostrum similique, enim saepe id fugiat temporibus aliquam culpa totam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur eveniet doloremque eius rerum impedit tempora accusamus saepe, incidunt, fugit atque voluptatem veritatis officia asperiores et? Ab tempora recusandae sunt amet?</p>
+
+        <ul>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+          <li>Lorem</li>
+        </ul>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, qui commodi, quia ea quo quasi atque fugit laboriosam cum sequi nostrum similique, enim saepe id fugiat temporibus aliquam culpa totam. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
     </div>
   )
 }
