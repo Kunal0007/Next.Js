fc2e9be248a83e993d03510fe36d146664f756b3
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-22 22:40:11 +0530

    Update

--- myblogs/pages/blogs.js
+++ myblogs/pages/blogs.js
@@ -32,36 +32,7 @@ const Blogs = (props) => {
           </Link>
         );
       })}
-      {/* <Link href='/blogpost/How to learn Next.js'>
-        <a className={styles.card}>
-          <h2>Blog 1 &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-      </Link>
-
-      <Link href='/blogpost/How to Integrate Java with SQL'>
-        <a className={styles.card}>
-          <h2>Blog 2 &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-      </Link>
-
-      <Link href='/blogpost/How to learn Cloud Computing'>
-        <a className={styles.card} >
-          <h2>Blog 3 &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-      </Link> 
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={styles.card}
-      >
-        <h2>Blog 4 &rarr;</h2>
-        <p>
-          Instantly deploy your Next.js site to a public URL with Vercel.
-        </p>
-      </a>*/}
+      
     </div>
   )
 }
