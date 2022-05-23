3d1181c40d4545955b2a5bccf91a862aef630236
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-05-23 12:35:20 +0530

    Last Commit

--- myblogs/pages/index.js
+++ myblogs/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to MY BLOG
+          MY BLOG
         </h1>
 
         <p className={styles.description}>
